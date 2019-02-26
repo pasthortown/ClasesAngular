75fbf5073777ad75496c0c099f7836eb7b0630f7
pasthortown <pastorchessmaster@gmail.com>
2019-02-26 09:19:54 -0500

    juego tres en raya completo

--- segundoproyecto/src/app/components/juego/juego.component.ts
+++ segundoproyecto/src/app/components/juego/juego.component.ts
@@ -49,7 +49,23 @@ export class JuegoComponent implements OnInit {
   }
 
   juegaPC() {
-    this.jugarAzar();
+    let jugue = false;
+    jugue = this.verificarCasillas('fas fa-angry', 'fas fa-angry');
+    if (!jugue) {
+      jugue = this.verificarCasillas('fas fa-times', 'fas fa-angry');
+    }
+    if (!jugue) {
+      jugue = this.TomarCentro('fas fa-angry');
+    }
+    if (!jugue) {
+      jugue = this.CuidadoDoble('fas fa-times', 'fas fa-angry');
+    }
+    if (!jugue) {
+      jugue = this.PriorizarEsquinas('fas fa-angry');
+    }
+    if (!jugue) {
+      this.jugarAzar();
+    }
     this.turno = 'Humano';
   }
 
@@ -150,6 +166,233 @@ export class JuegoComponent implements OnInit {
     }
     if (this.ganaPorFicha('fas fa-times')) {
       alert('Fin del Juego!, Ganaste');
+      return true;
+    }
+    return false;
+  }
+
+  verificarCasillas(ficha: string, fichaJuego: string) {
+    let cuenta = 0;
+    let columnaAtencion = 5;
+    let filaAtencion = 5;
+    // Verificación por filas
+    for (let fila = 0 ; fila < 3 ; fila++) {
+      for (let columna = 0; columna < 3 ; columna ++) {
+        if (this.tablero[fila][columna] === ficha) {
+          cuenta ++;
+        } else {
+          if (this.tablero[fila][columna] !== '') {
+            cuenta += 5;
+          } else {
+            columnaAtencion = columna;
+            filaAtencion = fila;
+          }
+        }
+      }
+      if ( cuenta === 2 ) {
+        this.seleccionarCasilla(fichaJuego, filaAtencion, columnaAtencion);
+        this.estaTableroLleno();
+        return true;
+      } else {
+        cuenta = 0;
+        columnaAtencion = 5;
+        filaAtencion = 5;
+      }
+    }
+    // Verificación por columnas
+    for (let columna = 0 ; columna < 3 ; columna++) {
+      for (let fila = 0; fila < 3 ; fila ++) {
+        if (this.tablero[fila][columna] === ficha) {
+          cuenta ++;
+        } else {
+          if (this.tablero[fila][columna] !== '') {
+            cuenta += 5;
+          } else {
+            columnaAtencion = columna;
+            filaAtencion = fila;
+          }
+        }
+      }
+      if ( cuenta === 2 ) {
+        this.seleccionarCasilla(fichaJuego, filaAtencion, columnaAtencion);
+        this.estaTableroLleno();
+        return true;
+      } else {
+        cuenta = 0;
+        columnaAtencion = 5;
+        filaAtencion = 5;
+      }
+    }
+    // VERIFICAR DIAGONALES
+    if (this.tablero[0][0] === ficha) {
+      cuenta++;
+    } else {
+      if (this.tablero[0][0] !== '') {
+        cuenta += 5;
+      } else {
+        columnaAtencion = 0;
+        filaAtencion = 0;
+      }
+    }
+    if (this.tablero[1][1] === ficha) {
+      cuenta++;
+    } else {
+      if (this.tablero[1][1] !== '') {
+        cuenta += 5;
+      } else {
+        columnaAtencion = 1;
+        filaAtencion = 1;
+      }
+    }
+    if (this.tablero[2][2] === ficha) {
+      cuenta++;
+    } else {
+      if (this.tablero[2][2] !== '') {
+        cuenta += 5;
+      } else {
+        columnaAtencion = 2;
+        filaAtencion = 2;
+      }
+    }
+    if ( cuenta === 2 ) {
+      this.seleccionarCasilla(fichaJuego, filaAtencion, columnaAtencion);
+      this.estaTableroLleno();
+      return true;
+    } else {
+      cuenta = 0;
+      columnaAtencion = 5;
+      filaAtencion = 5;
+    }
+
+    if (this.tablero[0][2] === ficha) {
+      cuenta++;
+    } else {
+      if (this.tablero[0][2] !== '') {
+        cuenta += 5;
+      } else {
+        columnaAtencion = 2;
+        filaAtencion = 0;
+      }
+    }
+    if (this.tablero[1][1] === ficha) {
+      cuenta++;
+    } else {
+      if (this.tablero[1][1] !== '') {
+        cuenta += 5;
+      } else {
+        columnaAtencion = 1;
+        filaAtencion = 1;
+      }
+    }
+    if (this.tablero[2][0] === ficha) {
+      cuenta++;
+    } else {
+      if (this.tablero[2][0] !== '') {
+        cuenta += 5;
+      } else {
+        columnaAtencion = 2;
+        filaAtencion = 2;
+      }
+    }
+    if ( cuenta === 2 ) {
+      this.seleccionarCasilla(fichaJuego, filaAtencion, columnaAtencion);
+      this.estaTableroLleno();
+      return true;
+    } else {
+      cuenta = 0;
+      columnaAtencion = 5;
+      filaAtencion = 5;
+    }
+    return false;
+  }
+
+  TomarCentro(fichaJuego) {
+    if (this.tablero[1][1] === '') {
+      this.seleccionarCasilla(fichaJuego, 1, 1);
+      this.estaTableroLleno();
+      return true;
+    }
+    return false;
+  }
+
+  PriorizarEsquinas(fichaJuego) {
+    if (this.tablero[0][0] === '') {
+      this.seleccionarCasilla(fichaJuego, 0, 0);
+      this.estaTableroLleno();
+      return true;
+    }
+    if (this.tablero[0][2] === '') {
+      this.seleccionarCasilla(fichaJuego, 0, 2);
+      this.estaTableroLleno();
+      return true;
+    }
+    if (this.tablero[2][0] === '') {
+      this.seleccionarCasilla(fichaJuego, 2, 0);
+      this.estaTableroLleno();
+      return true;
+    }
+    if (this.tablero[2][2] === '') {
+      this.seleccionarCasilla(fichaJuego, 2, 2);
+      this.estaTableroLleno();
+      return true;
+    }
+    return false;
+  }
+
+  CuidadoDoble(ficha, fichaJuego) {
+    const sorteo = Math.floor(Math.random() * 4);
+    let fila = 0;
+    let columna = 0;
+    if (this.tablero[0][0] === '' && this.tablero[0][1] === '' && this.tablero[0][2] === ficha &&
+        this.tablero[1][0] === '' && this.tablero[1][1] === fichaJuego && this.tablero[1][2] === '' &&
+        this.tablero[2][0] === ficha && this.tablero[2][1] === '' && this.tablero[2][2] === ''
+    ) {
+      switch (sorteo) {
+        case 0:
+          fila = 0;
+          columna = 1;
+          break;
+        case 1:
+          fila = 1;
+          columna = 0;
+          break;
+        case 2:
+          fila = 1;
+          columna = 2;
+          break;
+        case 3:
+          fila = 2;
+          columna = 1;
+          break;
+      }
+      this.seleccionarCasilla(fichaJuego, fila, columna);
+      this.estaTableroLleno();
+      return true;
+    }
+    if (this.tablero[0][0] === ficha && this.tablero[0][1] === '' && this.tablero[0][2] === '' &&
+        this.tablero[1][0] === '' && this.tablero[1][1] === fichaJuego && this.tablero[1][2] === '' &&
+        this.tablero[2][0] === '' && this.tablero[2][1] === '' && this.tablero[2][2] === ficha
+    ) {
+      switch (sorteo) {
+        case 0:
+          fila = 0;
+          columna = 1;
+          break;
+        case 1:
+          fila = 1;
+          columna = 0;
+          break;
+        case 2:
+          fila = 1;
+          columna = 2;
+          break;
+        case 3:
+          fila = 2;
+          columna = 1;
+          break;
+      }
+      this.seleccionarCasilla(fichaJuego, fila, columna);
+      this.estaTableroLleno();
       return true;
     }
     return false;
